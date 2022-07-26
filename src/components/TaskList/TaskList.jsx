d58c3b8ec861f6db0fd2a99003ebf22fad442e35
Savienne <Saviennes.mitchell@yahoo.com>
2022-07-26 11:20:37 -0400

    style progress

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -19,7 +19,7 @@ export default function TaskList({
   handleEditTaskButton}) 
 {
   return (
-    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper', }}>
       {tasks.map(task => 
         <ListItem key={task._id}>
           <ListItemAvatar>
