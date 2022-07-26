2df17c8ff5af30dc797cf366d52806f6f176f253
Zach Eggert <egger0a6@email.arizona.edu>
2022-07-26 11:19:02 -0400

    fix authorization for showing tasks

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import styles from './Home.module.css'
 import { useState } from 'react'
 import { validateFormCollection } from "../../services/profileService"
 import Quote from '../../components/Quote/Quote'
+import Login from '../../pages/Login/Login'
 
 // Components
 import AddTaskForm from '../../components/AddTaskForm/AddTaskForm'
@@ -14,7 +15,8 @@ const Home = ({
   handleAddTask, 
   handleDeleteTask,
   handleEditTask,
-  handleDeleteAll
+  handleDeleteAll,
+  handleSignupOrLogin
 }) => {
 
   const [edit, setEdit] = useState(false)
@@ -69,25 +71,34 @@ const Home = ({
   }
 
   return (
-    <main className={styles.container}>
-      <h1>Welcome, {user ? user.name : 'friend'}</h1>
-      <Quote />
-      <AddTaskForm 
-        formData={formData}
-        errors={errors}
-        edit={edit}
-        handleChange={handleChange}
-        handleSubmit={handleSubmit}
-        checkValidForm={checkValidForm}
-        handleClearForm={handleClearForm}
-      />
-      <TaskList 
-        tasks={tasks} 
-        handleDeleteTask={handleDeleteTask}
-        handleEditTaskButton={handleEditTaskButton}
-      />
-      <DeleteAllDialog handleDeleteAll={handleDeleteAll}/>
-    </main>
+    <>
+      {user ? 
+        <main className={styles.container}>
+          <h1>Welcome, {user ? user.name : 'friend'}</h1>
+          <Quote />
+          <AddTaskForm 
+            formData={formData}
+            errors={errors}
+            edit={edit}
+            handleChange={handleChange}
+            handleSubmit={handleSubmit}
+            checkValidForm={checkValidForm}
+            handleClearForm={handleClearForm}
+          />
+          <TaskList 
+            tasks={tasks} 
+            handleDeleteTask={handleDeleteTask}
+            handleEditTaskButton={handleEditTaskButton}
+          />
+          <DeleteAllDialog handleDeleteAll={handleDeleteAll}/>
+        </main>
+        :
+        <main>
+          <Login handleSignupOrLogin={handleSignupOrLogin}/>
+        </main>
+
+      }
+    </>
   )
 }
 
